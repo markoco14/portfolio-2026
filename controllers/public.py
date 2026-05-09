f44bbfe05ae292152ba03eea5547e0149e18102d
Mark O'Connor <mark.oconnor14@gmail.com>
2026-05-10 03:00:51 +0800

    add time controls to mock scheduleboard

--- controllers/public.py
+++ controllers/public.py
@@ -1,4 +1,5 @@
 import asyncio
+from datetime import date
 
 from fastapi import Request
 
@@ -11,19 +12,31 @@ async def home(request: Request):
         context={}
     )
 
+
 async def schedule_board(request: Request):
-
     query_params = request.query_params
-    date = query_params.get("date")
+    selected_date = query_params.get("selected_date")
+    try:
+        speed = float(query_params.get("speed", 0.5))
+    except (ValueError, TypeError):
+        speed = 0.5
 
-    if not date:
-        date = "2026-05-10"
+    speed = max(0, min(speed, 10))
 
-    await asyncio.sleep(0.5)
+    if not selected_date:
+        selected_date = date.today()
+    else:
+        selected_date = date.fromisoformat(selected_date)
+
+
+    await asyncio.sleep(0.2)
     
     students_only = query_params.get("students_only")
     if students_only:
+        await asyncio.sleep(speed)
+
         students = [1, 2, 3, 4]
+
         return templates.TemplateResponse(
             request=request,
             name="_students.html",
@@ -31,12 +44,21 @@ async def schedule_board(request: Request):
                 "students": students
             }
         )
+    
+    date_iso_calendar = selected_date.isocalendar()
+    year_week_number = date_iso_calendar.week
 
+    quick_date_buttons = []
+    for i in range(1, 7):
+        quick_date_buttons.append(date.fromisocalendar(selected_date.year, year_week_number, i))
 
     return templates.TemplateResponse(
         request=request,
         name="schedule-board.html",
         context={
-            "date": date
+            "selected_date": selected_date.strftime("%Y-%m-%d"),
+            "header_date": selected_date.strftime("%A %B %d, %Y"),
+            "quick_date_buttons": quick_date_buttons,
+            "speed": speed
         }
     )
