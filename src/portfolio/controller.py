123a04769235b98103b6dc04c4e37b29ed99283f
Mark O'Connor <mark.oconnor14@gmail.com>
2026-05-26 00:01:30 +0800

    set up basic email/password check with db

--- src/portfolio/controller.py
+++ src/portfolio/controller.py
@@ -1,8 +1,10 @@
 from fastapi import Request, Response
 
+from src.database import get_conn
 from src.config import templates
 from src.vendor.auth.crypto import verify_password
 from src.vendor.auth.validators import is_valid_email
+
 
 async def home(request: Request):
     return templates.TemplateResponse(
@@ -10,6 +12,7 @@ async def home(request: Request):
         name="index.html",
         context={}
     )
+
 
 async def signin(request: Request):
     form_data = await request.form()
@@ -23,16 +26,27 @@ async def signin(request: Request):
             name="_form_response.html",
             status_code=422,
             context={
-                "unprocessable_error": "Something is wrong with your email or password"
+                "unprocessable_error": "Something is wrong with your email or password",
+                "email_error": "",
+                "password_error": ""
             }
         )
     
-    email_error = None
+    email_error = ""
+    user = None
     if not is_valid_email(email=email):
         email_error = "Invalid email"
+    else:
+        with get_conn() as conn:
+            user = conn.execute(
+                "SELECT user_id, email, hashed_password FROM users WHERE email = :email",
+                {"email": email}
+                ).fetchone()
+        if not user:
+            email_error = "Invalid email"
     
-    password_error = None
-    if not verify_password(password=password, hashed="#"):
+    password_error = ""
+    if user and not verify_password(password=password, hashed=user["hashed_password"]):
         password_error = "Invalid password"
 
     if email_error or password_error:
@@ -40,9 +54,21 @@ async def signin(request: Request):
             request=request,
             name="_form_response.html",
             context={
+                "unprocessable_error": "",
                 "email_error": email_error,
                 "password_error": password_error
             }
         )
 
-    return Response(status_code=200, content="Signed in")
+    return templates.TemplateResponse(
+        request=request,
+        name="_form_response.html",
+        context={
+            "unprocessable_error": "",
+            "email_error": "",
+            "password_error": ""
+        },
+        headers={
+            "hx-refresh": "true"
+        }
+    )
