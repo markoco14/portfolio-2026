2308932b5b8d2b1a9531ac98d36e8eb411d782bb
Mark O'Connor <mark.oconnor14@gmail.com>
2026-05-26 13:55:00 +0800

    add env variables for google sheet migration

--- src/config.py
+++ src/config.py
@@ -1,4 +1,12 @@
+from dotenv import load_dotenv
+import os
+
 from fastapi.templating import Jinja2Templates
 
+load_dotenv()
+
+USER_EMAIL = os.environ.get("USER_EMAIL")
+GOOGLE_SHEET = os.environ.get("GOOGLE_SHEET")
 
 templates = Jinja2Templates(directory="src/templates")
+
