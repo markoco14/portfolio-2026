becfbbabcb08aa7e25ecd4f1f4c7d05fa2593663
Mark O'Connor <mark.oconnor14@gmail.com>
2026-09-13 22:33:41 +0800

    create strength log table

---
+++ src/sql/migrations/20260913_02_8vtRz-create-strength-log-table.sql
@@ -0,0 +1,15 @@
+-- Create strength log table 
+-- depends: 20260913_01_af0E5-create-exercises-table
+
+CREATE TABLE strength_log(
+    entry_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    exercise_id INTEGER NOT NULL,
+    activity_date DATE NOT NULL,
+    reps INTEGER NOT NULL,
+    side TEXT CHECK (side IN ("left", "right")) DEFAULT NULL,
+    units TEXT,
+    amount REAL,
+    band TEXT CHECK (band IN ("red")) DEFAULT NULL,
+    created_at DATETIME DEFAULT (datetime('now')),
+    updated_at DATETIME
+);
